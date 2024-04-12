fc42ec80e3c4664fc8429bdf390fd3fd37c9b524
kimsjae <stwin755@gmail.com>
2024-04-12 15:49:09 +0900

    플러터 앱 수업

--- login_app/lib/main.dart
+++ login_app/lib/main.dart
@@ -10,10 +10,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute: "/login",
       routes: {
-        "/login" : (context) => LoginPage(),
-        "/home" : (context) => HomePage()
+        "/login": (context) => LoginPage(),
+        "/home": (context) => HomePage()
       },
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("Login")),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: ElevatedButton(
+          child: Text("move login page"),
+          onPressed: () {
+            Navigator.pushNamed(context, "/login");
+          },
+        ),
+      ),
     );
   }
 }
